c179930c408665035a0768400b656410bb2ec0a8
sameir13 <sameeraadil.x@gmail.com>
2023-07-28 11:02:22 -0700

    added a new footer in the website

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,53 +1,99 @@
-
-
-
-
-
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <section className="MainFooterSection MaxFooter">
-
-        <div className="bannerMainProducts">
-
-           <div className="bannerDiv">
-            <img src="/images/banner`.svg" alt="Footer Banner Ecommerce" />
-            <div className="bannerdivtext">
-                <h5>PTA Approved</h5>
-                <p>Mobile Phone</p>
-            </div>
-           </div>
-
-           <div className="bannerDiv">
-            <img src="/images/banner2.svg" alt="Footer Banner Ecommerce" />
-            <div className="bannerdivtext">
-                <h5>1 Year</h5>
-                <p>Brand Warranty</p>
-            </div>
-           </div>
-
-            <div className="bannerDiv">
-            <img src="/images/banner3.svg" alt="Footer Banner Ecommerce" />
-            <div className="bannerdivtext">
-                <h5>48hr Delivery</h5>
-                <p>Open Parcel</p>
-            </div>
-           </div>
-
-            <div className="bannerDiv">
-            <img src="/images/banner4.svg" alt="Footer Banner Ecommerce" />
-            <div className="bannerdivtext">
-                <h5>Free Delivery</h5>
-                <p>All Over Pakistan</p>
-            </div>
-           </div>
+      <div className="bannerMainProducts">
+        <div className="bannerDiv">
+          <img src="/images/banner`.svg" alt="Footer Banner Ecommerce" />
+          <div className="bannerdivtext">
+            <h5>PTA Approved</h5>
+            <p>Mobile Phone</p>
+          </div>
         </div>
 
-        <footer className="ProductLandingPageFooter">
+        <div className="bannerDiv">
+          <img src="/images/banner2.svg" alt="Footer Banner Ecommerce" />
+          <div className="bannerdivtext">
+            <h5>1 Year</h5>
+            <p>Brand Warranty</p>
+          </div>
+        </div>
 
-        </footer>
+        <div className="bannerDiv">
+          <img src="/images/banner3.svg" alt="Footer Banner Ecommerce" />
+          <div className="bannerdivtext">
+            <h5>48hr Delivery</h5>
+            <p>Open Parcel</p>
+          </div>
+        </div>
+
+        <div className="bannerDiv">
+          <img src="/images/banner4.svg" alt="Footer Banner Ecommerce" />
+          <div className="bannerdivtext">
+            <h5>Free Delivery</h5>
+            <p>All Over Pakistan</p>
+          </div>
+        </div>
+      </div>
+
+      <footer className="ProductLandingPageFooter">
+        <div className="FooterDivCol1">
+          <div className="footerMainLogo">
+            <img src="/images/footerlogo.svg" alt="" />
+          </div>
+
+          <div className="footerMainCol1Links">
+            <Link href={"/"}>About us</Link>
+            <Link href={"/"}>FAQs</Link>
+            <Link href={"/"}>Contact Us</Link>
+            <Link href={"/"}>Careers</Link>
+            <Link href={"/"}>Press & Blog</Link>
+            <Link href={"/"}>Terms & Condition</Link>
+          </div>
+        </div>
+
+        <div className="FooterDivCol1">
+          <div className="footerMainLogo">
+            <p>Customer Service</p>
+          </div>
+
+          <div className="footerMainCol1Links">
+            <Link href={"/"}>Help Center</Link>
+            <Link href={"/"}>Privacy Policy</Link>
+            <Link href={"/"}>Installments Plan</Link>
+            <Link href={"/"}>Careers</Link>
+            <Link href={"/"}>E-Warranty Activation</Link>
+          </div>
+        </div>
+
+        <div className="FooterDivCol1">
+          <div className="FooterPaymentDiv">
+            <p>Secure Payments Methods</p>
+            <img
+              src="https://static.priceoye.pk/images/payment_method.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </footer>
+
+      <div className="FooterMainHelpIcons">
+        <div className="helpMainDiv1">
+          <p>PriceOye.pk</p>
+        </div>
+        <div className="helpMainDiv1">
+          <i class="fa-brands fa-youtube"></i>
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-tiktok"></i>
+        </div>
+        <div className="helpMainDiv1" style={{ visibility: "hidden" }}>
+          <p>euhuhssssssssssssssss</p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css" integrity="sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css" integrity="sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
